179459cdad46f3e2517aa8f68f00d2576ae2bfc0
illaryhuaylupoGL <illary.huaylupo@gorillalogic.com>
2020-06-05 09:29:52 -0600

    Fix useEffect call

--- src/App.js
+++ src/App.js
@@ -10,13 +10,11 @@ function App(){
   const [username, setUsername] = useState(null);
 
   useEffect(() => {
-  
       Auth.currentAuthenticatedUser({
         bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
     }).then(user => setUsername(Auth.user.attributes.email))
     .catch(err => console.log(err));
-    
-  });
+  },[]);
 
     return (
       
